f3b7b7f4da7450111085e754220948bc6b1d4c36
hasanelmestrah <hasanelmestrah@gmail.com>
2023-01-09 17:01:37 +0200

    step 3

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`successfully connect at http://localhost:${port}`)
 })
+
+////
+
+app.get('/test', (req, res) => {
+  res.status(200).send({ status: 200, message: 'ok' });
+});
+
+
+app.get('/time', (req, res) => {
+      const currentTime = new Date();
+      const hours = currentTime.getHours();
+      const minutes = currentTime.getMinutes();
+      const timeString = `${hours}:${minutes}`;
+    
+      res.status(200).send({ status: 200, message: timeString });
+    });
+    
